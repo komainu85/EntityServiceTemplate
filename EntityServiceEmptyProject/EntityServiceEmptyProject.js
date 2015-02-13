0e69117032f11aad3f91d14a9aee16d25e658c04
sobek1985 <sobek1985@gmail.com>
2015-02-13 12:46:38 +0000

    Init Commit

---
+++ EntityServiceEmptyProject/EntityServiceEmptyProject.js
@@ -0,0 +1,23 @@
+﻿require.config({
+    paths: {
+        entityService: "/sitecore/shell/client/Services/Assets/lib/entityservice"
+    }
+});
+
+define(["sitecore", "jquery", "underscore", "entityService"], function (Sitecore, $, _, entityService) {
+    var EntityServiceEmptyProject = Sitecore.Definitions.App.extend({
+
+        filesUploaded: [],
+
+        initialized: function () {
+        },
+
+        initialize: function () {
+
+        },
+
+
+    });
+
+    return EntityServiceEmptyProject;
+});
